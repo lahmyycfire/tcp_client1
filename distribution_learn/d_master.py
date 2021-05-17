5a5808bc26638157d766b34fc93a96e18230e084
lahmyycfire <546090788@qq.com>
2021-05-17 17:11:39 +0800

    分布式进程的master端

---
+++ distribution_learn/d_master.py
@@ -0,0 +1,43 @@
+import queue, random, time
+from multiprocessing.managers import BaseManager
+
+task_queue = queue.Queue()
+result_queue = queue.Queue()
+
+
+def return_task_queue():
+    global task_queue
+    return task_queue
+
+
+def return_result_queue():
+    global result_queue
+    return result_queue
+
+
+class QueueManager(BaseManager):
+    pass
+
+if __name__ == '__main__':
+    QueueManager.register('get_task_queue', callable=return_task_queue)
+    QueueManager.register('get_result_queue', callable=return_result_queue)
+
+    manager = QueueManager(address=('127.0.0.1', 5000), authkey=b'abc')
+    manager.start()
+
+    task = manager.get_task_queue()
+    result = manager.get_result_queue()
+
+    for i in range(10):
+        n = random.randint(0, 10000)
+        print('Put task %d' % n)
+        task.put(n)
+
+    print('try get result')
+    for i in range(10):
+        r = result.get(timeout=10)
+        print('result:%s' % r)
+
+    manager.shutdown()
+    print('manager exit')
+
